771181f32807f8cd37242305be490c8d6eba39e4
Dimady <romashka14108@mail.ru>
2022-04-21 13:49:49 +0300

    first commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "otus-hdfs-homework"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies ++= Seq(
+  "com.typesafe" % "config" % "1.4.2",
+  "org.apache.hadoop" % "hadoop-client" % "3.2.1"
+)
---
+++ src/main/scala/ru/otus/homework/hdfs/HdfsService.scala
@@ -0,0 +1,45 @@
+package ru.otus.homework.hdfs
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.io.IOUtils
+
+object HdfsService {
+
+  def listDirs(root: Path)(implicit fs: FileSystem): Array[Path] =
+    fs.listStatus(root).filter(_.isDirectory).map(_.getPath)
+
+  def getFirstFileName(root: Path)(implicit fs: FileSystem): Option[Path] = {
+    val files = fs.listStatus(root).filterNot(_.isDirectory).map(_.getPath)
+    if (files.nonEmpty) Some(files.minBy(_.getName)) else None
+  }
+
+  def copyMergeFiles(srcDir: Path, dstDirName: String, conf: Configuration)(implicit fs: FileSystem): Boolean = {
+    val files = fs.listStatus(srcDir).filter(_.isFile).map(_.getPath).filterNot(_.getName == ".DS_Store")
+    val targetFileName = if (files.nonEmpty) Some(files.minBy(_.getName)) else None
+    val targetDirName = s"$dstDirName/${srcDir.getName}"
+
+    if (targetFileName.isDefined) {
+      val out = fs.create(new Path(s"$targetDirName/${targetFileName.get.getName}"))
+      try {
+        files.foreach(file => {
+          val in = fs.open(file)
+          println("writing file")
+          try {
+            IOUtils.copyBytes(in, out, conf, false)
+          } finally {
+            in.close()
+          }
+        })
+      } finally {
+        out.close()
+      }
+    }
+
+    fs.delete(srcDir, true)
+  }
+
+  def move(root: Path, target: String, conf: Configuration)(implicit fs: FileSystem): Unit = {
+    listDirs(root).foreach(copyMergeFiles(_, target, conf))
+  }
+}
